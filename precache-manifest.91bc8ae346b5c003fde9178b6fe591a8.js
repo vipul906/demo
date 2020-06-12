b2235c58f93d3ed4ccfe4329060d3454c378f484
vipul <vipuljain906@gmail.com>
2020-06-12 18:30:30 +0530

    Updates

---
+++ precache-manifest.91bc8ae346b5c003fde9178b6fe591a8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9337a23f2fbd0707374f674f6b21e804",
+    "url": "/demo/index.html"
+  },
+  {
+    "revision": "eceeb2f5841f7bbe0b61",
+    "url": "/demo/static/css/main.4f063ed2.chunk.css"
+  },
+  {
+    "revision": "c38007060728897ea70d",
+    "url": "/demo/static/js/2.fd344179.chunk.js"
+  },
+  {
+    "revision": "5afd144ed6795b85094a542d346dbbf0",
+    "url": "/demo/static/js/2.fd344179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "eceeb2f5841f7bbe0b61",
+    "url": "/demo/static/js/main.4d32c299.chunk.js"
+  },
+  {
+    "revision": "95c20c9465d9a5f121bd",
+    "url": "/demo/static/js/runtime-main.aa309d8a.js"
+  }
+]);
